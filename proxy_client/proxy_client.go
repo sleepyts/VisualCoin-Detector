24d0728b309c11c73a751bf18155f1103ffdae72
sleepyts <choujack373@gmail.com>
2025-05-10 14:26:47 +0800

    feat: proxy config load

--- proxy_client/proxy_client.go
+++ proxy_client/proxy_client.go
@@ -3,6 +3,7 @@ package proxy_client
 import (
 	"net/http"
 	"net/url"
+	"strconv"
 
 	"gihub.com/sleepyts/Visual_Coin_Detector/config"
 )
@@ -11,7 +12,7 @@ var Client *http.Client
 
 func InitClient() {
 	if config.UseProxy() {
-		proxyUrl, _ := url.Parse("http://127.0.0.1:7897")
+		proxyUrl, _ := url.Parse(config.AppConfig.Proxy.ProxyURL + ":" + strconv.Itoa(config.AppConfig.Proxy.ProxyPort))
 		Client = &http.Client{
 			Transport: &http.Transport{
 				Proxy: http.ProxyURL(proxyUrl),
